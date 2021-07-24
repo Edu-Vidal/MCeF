4b0cf202e7c74f9f733cae35c4e1389f570a1bb7
Edu-Vidal <e.vidal.aragao@uol.com.br>
2021-07-24 19:54:12 -0300

    feat: Implementa funções e wrappers

---
+++ Cap2/Ex_2.1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#include "integral.cpp"
+
+// f(x) = sqrt(1-a²)
+double f(double a){
+    return (std::sqrt(1-(a*a)));
+};
+
+// f(x) = -x² + 1
+double aprox_f(double a){
+    return (-(a*a)+1);
+};
+
+double integrationLimits[2] {0, 1};
+int numberOfIntervals = 100000;
+
+using namespace std::chrono;
+
+int main() {
+    // EX 2.1 - 1
+    auto start = high_resolution_clock::now();
+    double area = Integral::compute(f, integrationLimits, numberOfIntervals);
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    std::cout << "2.1-1 Resultado: " << area << std::endl;
+    std::cout << "Tempo gasto: " << duration.count() << " microsegundos" << "\n\n";
+
+    // EX 2.1 - 2
+    start = high_resolution_clock::now();
+    area = Integral::computeMedia(f, integrationLimits, numberOfIntervals);
+    stop = high_resolution_clock::now();
+
+    duration = duration_cast<microseconds>(stop - start);
+
+    std::cout << "2.1-2 Resultado: " << area << std::endl;
+    std::cout << "Tempo gasto: " << duration.count() << " microsegundos" << "\n\n";
+
+    // EX 2.1 - 3
+    start = high_resolution_clock::now();
+    area = Integral::computeMedia(aprox_f, integrationLimits, numberOfIntervals);
+    stop = high_resolution_clock::now();
+
+    duration = duration_cast<microseconds>(stop - start);
+
+    std::cout << "2.1-3 Resultado: " << area << std::endl;
+    std::cout << "Tempo gasto: " << duration.count() << " microsegundos" << std::endl;
+
+};
