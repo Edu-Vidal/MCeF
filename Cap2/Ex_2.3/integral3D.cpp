afe3b0cd877c7844fe30cd4b3f8bc7055b63d506
Edu-Vidal <e.vidal.aragao@uol.com.br>
2021-07-24 22:38:22 -0300

    feat: implementa função para calcular integral

---
+++ Cap2/Ex_2.3/integral3D.cpp
@@ -0,0 +1,20 @@
+#include "integral3D.hpp"
+
+double Integral::somaParalelepipedosMedia(
+    Expression f,
+    double integrationLimitsX[], 
+    double integrationLimitsY[],
+    int numberOfDivisions)
+{
+    double integral = 0;
+    double dx = (integrationLimitsX[1] - integrationLimitsX[0]) / numberOfDivisions;
+    double dy = (integrationLimitsY[1] - integrationLimitsY[0]) / numberOfDivisions;
+    for (int i = 1; i <= numberOfDivisions; i++)
+    {   
+        for (int j = 1; j <= numberOfDivisions; j++)
+            integral += (f(integrationLimitsX[0]+i*dx, integrationLimitsY[0]+j*dy)\
+            +f(integrationLimitsX[0]+(i-1)*dx, integrationLimitsY[0]+(j-1)*dy))/2\
+            * dx * dy;
+    };
+    return integral;
+};
