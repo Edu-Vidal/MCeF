46973e95704776ea07689eafb694dd0d0228402c
Eduardo Vidal Aragão <e.vidal.aragao@uol.com.br>
2021-07-29 18:07:10 -0300

    feat: estrutura inputs e chamada de funcoes

---
+++ Cap2/Ex_2.4/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#include "integral.cpp"
+
+// f(x, y) = e^(0.1(x²+y²))
+double f(double x, double y){
+    return exp(0.1 * (x * x + y * y));
+};
+
+// f(x) = -x² + 1
+double aprox_f(double x){
+    return (-(x*x)+1);
+};
+
+// Em x [-1, 1], em y [-1, 1]
+double integrationLimitsX[] {-1, 1};
+double integrationLimitsY[] {-1, 1};
+int numberOfIntervals = 1000;
+
+using namespace std::chrono;
+
+int main(){
+    auto start = high_resolution_clock::now();
+    double volume = Integral::monteCarlo(
+        f, 
+        integrationLimitsX, 
+        integrationLimitsY, 
+        numberOfIntervals);
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    std::cout << "2.3-1 Resultado: " << volume << std::endl;
+    std::cout << "Tempo gasto: " << duration.count() << " microsegundos" << "\n\n";
+}
