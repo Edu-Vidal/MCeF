bce7f7d50f84c81696105c44dd145ff803323502
Edu-Vidal <e.vidal.aragao@uol.com.br>
2021-09-08 21:34:09 -0300

    docs

--- Cap2/Ex_2.4/main.cpp
+++ Cap2/Ex_2.4/main.cpp
@@ -4,32 +4,35 @@
 
 #include "integral.cpp"
 
-// f(x) = -x² + 1
+// Declaração de funções -------
+// f(x) = sqrt(1-x²)
 double f(double x){
-    return (-(x*x)+1);
+    return (std::sqrt(1-(x*x)));
 };
 
 // f(x, y) = e^(0.1(x²+y²))
 double exponential_f(double x, double y){
     return exp(0.1 * (x * x + y * y));
 };
+//------------------------------
 
+// Declaração dos limites de integração
 double f_limits[2] {0, 1};
-int numberOfIntervals = 1000;
+
+int numberOfDivisions = 1000000;
 
 using namespace std::chrono;
 
 int main(){
     auto start = high_resolution_clock::now();
-    double volume = Integral::monteCarlo(
+    double volume = Integral::monteCarlo2D(
         f, 
         f_limits,
-        f_limits,
-        numberOfIntervals);
+        numberOfDivisions);
     auto stop = high_resolution_clock::now();
 
     auto duration = duration_cast<microseconds>(stop - start);
 
-    std::cout << "2.3-1 Resultado: " << volume << std::endl;
+    std::cout << "2.4-1 Resultado: " << volume << std::endl;
     std::cout << "Tempo gasto: " << duration.count() << " microsegundos" << "\n\n";
 }
